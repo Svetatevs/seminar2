3c51f9bde73ffa9754ad0716c3dcb4d2f87f5cba
Svetatevs <urusel.k@mail.ru>
2022-08-26 16:21:42 +0300

    Большая цифра рандомного числа

---
+++ zadacha9/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+9.Напишите программу, которая выводит случайное число из отрезка [10,99] 
+и показывает наибольшую цифру числа.
+*/
+Random random = new Random();
+int randomNumber = random.Next(10,100);
+
+Console.WriteLine(randomNumber);
+
+int firstNumber = randomNumber / 10;
+
+int lastNumber = randomNumber % 10;
+
+if(lastNumber > firstNumber)
+{
+    Console.WriteLine($"Максимальное число равно {lastNumber}");
+}
+else
+{
+ Console.WriteLine($"Максимальное число равно {firstNumber}");
+}
