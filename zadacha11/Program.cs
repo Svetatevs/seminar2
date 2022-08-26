69e3f8317e16e0123c63d64d6c63178bd01b9951
Svetatevs <urusel.k@mail.ru>
2022-08-26 20:37:34 +0300

    Удаляем вторую цифру трёхзначного числа

---
+++ zadacha11/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая выводит случайное трехзначное число
+ и удаляет вторую цифру этого числа. 456-46, 782-72, 918-98.
+ */
+
+ Random random = new Random();
+int randomNumber = random.Next(100,1000);
+
+Console.WriteLine(randomNumber);
+
+int firstNumber = randomNumber / 100;
+
+int lastNumber = randomNumber % 10;
+
+Console.Write(firstNumber);
+Console.Write(lastNumber);
+/*
+
+int Number = new Random().Next(100,1000);
+Console.WriteLine(Number);
+*/
